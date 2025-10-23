f153b2cd3b1fa66357907a412f7b7909d9242b41
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-10-23 17:19:41 +0200

    Adding Grid.js lib and BookTable component.

---
+++ src/lib/components/BookTable.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { onMount } from 'svelte';
+	import { Grid, h } from 'gridjs';
+	import 'gridjs/dist/theme/mermaid.css';
+
+	export let books = [];
+
+	let bookTableWrapper;
+
+	onMount(() => {
+		new Grid({
+			columns: [
+				'#',
+				'Title',
+				'Author',
+				'Publisher',
+				'ISBN',
+				{ id: 'publish-year', name: 'Publish year' },
+				'Edition',
+				'Language',
+				{ id: 'reading-status', name: 'Reading status' }
+			],
+			search: true,
+			sort: true,
+			pagination: {
+				enabled: true,
+				limit: 15
+			},
+			data: (books || []).map((book, index) => [
+				index + 1,
+				book.title,
+				(book.authors || []).join(', '),
+				book.publisher_name,
+				book.isbn,
+				book.publish_year,
+				book.edition,
+				book.language_name,
+				book.reading_status
+			]),
+			style: {
+				th: {
+					'text-align': 'center'
+				}
+			}
+		}).render(bookTableWrapper);
+	});
+</script>
+
+<div bind:this={bookTableWrapper}></div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,70 +1,15 @@
 <script>
+	import BookTable from '$lib/components/BookTable.svelte';
 	export let data;
 </script>
 
 <h1 data-testid="title">&#128218; Shelves</h1>
 
-<table data-testid="books-table">
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Title</th>
-			<th>Author</th>
-			<th>Publisher</th>
-			<th>ISBN</th>
-			<th>Publish year</th>
-			<th>Edition</th>
-			<th>Language</th>
-			<th>Reading status</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each data.books as book, index}
-			<tr>
-				<td>{index + 1}</td>
-				<td>{book.title}</td>
-				<td>{book.authors.join(', ')}</td>
-				<td>{book.publisher_name}</td>
-				<td>{book.isbn}</td>
-				<td>{book.publish_year}</td>
-				<td>{book.edition}</td>
-				<td>{book.language_name}</td>
-				<td>{book.reading_status}</td>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+<BookTable books={data.books} />
 
 <style>
 	:global(body) {
 		font-family: Arial, sans-serif;
 		margin: 20px;
-	}
-	table {
-		width: 100%;
-		border-collapse: collapse;
-	}
-
-	th,
-	tr:nth-child(even) {
-		background-color: #f2f2f2;
-	}
-
-	/* book title */
-	td:nth-child(2) {
-		font-weight: bold;
-	}
-
-	th,
-	td:nth-child(1),
-	td:nth-child(6),
-	td:nth-child(7) {
-		text-align: center;
-	}
-
-	th,
-	td {
-		border: 1px solid #ddd;
-		padding: 8px;
 	}
 </style>
