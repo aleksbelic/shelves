a8adac99aab988ba02e284ed32e2da26dc3bab8b
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-10-26 02:15:56 +0200

    Styling badges, adding category.

--- src/lib/components/BookTable.svelte
+++ src/lib/components/BookTable.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
 	import {
+		Badge,
 		Table,
 		TableBody,
 		TableBodyCell,
 		TableBodyRow,
 		TableHead,
-		TableHeadCell
+		TableHeadCell,
+		P
 	} from 'flowbite-svelte';
 
 	export let books: any[] = [] as any[];
 
-	// TODO: use only {items} object and not books directly: https://flowbite-svelte.com/docs/components/table#table-from-data
+	function getBadgeColorForGenre(genre: string) {
+		switch (genre.toLowerCase()) {
+			case 'scy-fi':
+				return 'orange';
+			case 'horror':
+				return 'teal';
+			case 'thriller':
+				return 'purple';
+			case 'romance':
+				return 'pink';
+			default:
+				return 'gray';
+		}
+	}
 </script>
 
 <Table striped={true} hoverable={true}>
@@ -18,6 +33,7 @@
 		<TableHeadCell>#</TableHeadCell>
 		<TableHeadCell>Title</TableHeadCell>
 		<TableHeadCell>Author</TableHeadCell>
+		<TableHeadCell>Category</TableHeadCell>
 		<TableHeadCell>Publisher</TableHeadCell>
 		<TableHeadCell>ISBN</TableHeadCell>
 		<TableHeadCell>Publish year</TableHeadCell>
@@ -29,8 +45,17 @@
 		{#each books as book, index}
 			<TableBodyRow>
 				<TableBodyCell>{index + 1}</TableBodyCell>
-				<TableBodyCell>{book.title}</TableBodyCell>
+				<TableBodyCell><P weight="medium">{book.title}</P></TableBodyCell>
 				<TableBodyCell>{book.authors.join(', ')}</TableBodyCell>
+				<TableBodyCell>
+					{#each book.categories as category}
+						{#if category !== null}
+							<Badge rounded class="ms-2 font-semibold" color={getBadgeColorForGenre(category)}
+								>{category}</Badge
+							>
+						{/if}
+					{/each}
+				</TableBodyCell>
 				<TableBodyCell>{book.publisher_name}</TableBodyCell>
 				<TableBodyCell>{book.isbn}</TableBodyCell>
 				<TableBodyCell>{book.publish_year}</TableBodyCell>
