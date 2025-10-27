d97a5b0daef6c54a8b17a6b361b679682812dc7f
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-10-27 17:18:16 +0100

    Adding new logo.

--- src/lib/components/MyNavbar.svelte
+++ src/lib/components/MyNavbar.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <div class="mb-4 flex items-start justify-between">
-	<span></span>
 	<img
-		src="/images/shelves-logo.png"
-		class="mr-2 h-10 w-auto sm:h-10 md:h-12 lg:h-16"
+		src="/images/shelves-logo.jpg"
+		class="mr-2 h-20 w-auto sm:h-20 md:h-22 lg:h-26"
 		alt="Shelves logo"
 	/>
 	<DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-600" />
