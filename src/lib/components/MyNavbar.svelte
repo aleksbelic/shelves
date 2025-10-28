1bd6850699e8d5013aa692c17bba426100b60de1
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-10-28 02:16:39 +0100

    Adding Flowbite icons & chart plugin. Adding pie-chart icon.

--- src/lib/components/MyNavbar.svelte
+++ src/lib/components/MyNavbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { DarkMode } from 'flowbite-svelte';
+	import { Button, DarkMode } from 'flowbite-svelte';
+	import { ChartPieOutline } from 'flowbite-svelte-icons';
 </script>
 
 <div class="mb-4 flex items-start justify-between">
@@ -9,5 +10,12 @@
 		class="mr-2 h-24 w-auto sm:h-24 md:h-26 lg:h-30"
 		alt="Shelves logo"
 	/>
-	<DarkMode class="text-primary-500 dark:text-primary-600 border dark:border-gray-600" />
+	<div>
+		<Button
+			class="bg-0 border border-primary-500 p-2.5 hover:bg-gray-100 focus:ring-0 dark:border-gray-600 dark:bg-gray-800 dark:hover:bg-gray-700"
+		>
+			<ChartPieOutline class="text-primary-500 dark:text-primary-600" />
+		</Button>
+		<DarkMode class="border text-primary-500 dark:border-gray-600 dark:text-primary-600" />
+	</div>
 </div>
