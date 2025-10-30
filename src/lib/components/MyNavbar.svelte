84183a24f2e5ebe02da37f7765d2685424849d8b
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2025-10-30 17:26:55 +0100

    Theme picker is now absolute.

--- src/lib/components/MyNavbar.svelte
+++ src/lib/components/MyNavbar.svelte
@@ -2,14 +2,13 @@
 	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<div class="mb-4 flex items-start justify-between">
-	<span></span>
+<div class="relative flex items-center justify-center">
 	<img
 		src="/images/shelves-logo.png"
-		class="mr-2 h-24 w-auto sm:h-24 md:h-26 lg:h-30"
+		class="h-24 w-auto sm:h-24 md:h-26 lg:h-30"
 		alt="Shelves logo"
 	/>
-	<div>
+	<div class="absolute top-0 right-0">
 		<DarkMode class="border text-primary-500 dark:border-gray-600 dark:text-primary-600" />
 	</div>
 </div>
